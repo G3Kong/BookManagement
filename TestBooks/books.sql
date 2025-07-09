d60deb24225f38afddeb2986120715dd2e36a607
G3Kong <aiueo91381412@gmail.com>
2025-07-09 16:28:33 +0900

    Add files via upload

---
+++ TestBooks/books.sql
@@ -0,0 +1,19 @@
+-- 1冊目
+INSERT INTO books (title, author, genre, publish_year, memo)
+VALUES ('君の名は。', '新海誠', '小説', 2016, '映画原作の小説');
+
+-- 2冊目
+INSERT INTO books (title, author, genre, publish_year, memo)
+VALUES ('ハリー・ポッターと賢者の石', 'J.K.ローリング', 'ファンタジー', 1997, 'シリーズ第1巻');
+
+-- 3冊目
+INSERT INTO books (title, author, genre, publish_year, memo)
+VALUES ('Javaの基礎', '山田太郎', '技術書', 2021, '初心者向けJava入門書');
+
+-- 4冊目
+INSERT INTO books (title, author, genre, publish_year, memo)
+VALUES ('進撃の巨人 1巻', '諫山創', 'マンガ', 2009, 'ダークファンタジーの代表作');
+
+-- 5冊目
+INSERT INTO books (title, author, genre, publish_year, memo)
+VALUES ('ノルウェイの森', '村上春樹', '文学', 1987, '村上春樹の代表作');
---
+++ TestBooks/demo/src/main/java/com/books/demo/controller/BookItemShowController.java
@@ -0,0 +1,9 @@
+package com.books.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import java.util.List;
+
+@Controller
+public class BookItemShowController {
+    
+}
